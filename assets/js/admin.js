3cda8e64817ce5dfa08e1ec72a2de979fa04ca81
RDSergij <sergijlys@gmail.com>
2016-01-14 11:29:44 +0200

    Style guide

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,6 +4,21 @@
 jQuery( document ).ready( initWidget );
 jQuery( document ).on( 'widget-updated widget-added ready', initWidget );
 
+
+
+function refreshForm( el ) {
+	el.val( ( new Date ).getTime() );
+
+	// el.trigger( 'change' );
+	el.focus();
+	el.trigger( { type: 'keydown', which: 13 } );
+	jQuery( document ).trigger( 'widget-updated' );
+	el.change( function( e ) {
+		e.preventDefault();
+		jQuery( document ).trigger( 'widget-updated' );
+	} );
+};
+
 /**
  * Initialization widget js
  *
@@ -12,14 +27,11 @@ jQuery( document ).on( 'widget-updated widget-added ready', initWidget );
 function initWidget() {
 	window.CHERRY_API.ui_elements.switcher.init( jQuery( 'body' ) );
 	jQuery( '.sortable' ).sortable();
-	jQuery( '.sortable' ).on( 'sortchange', function( event, ui ) {
-		el = ui.item.parents( '.tm-categories-tiles-form-widget' ).find( 'input.sort-is' );
-		refreshForm = function( el ) {
-			el.val( ( new Date ).getTime() ).focus().trigger( { type: 'keydown', which: 13 } );
-			el.trigger( 'change' );
-		};
 
-		setTimeout( refreshForm( el ), 2000 );
+	 jQuery( '.sortable' ).on( 'sortstop', function( event, ui ) {
+		el = ui.item.parents( '.tm-categories-tiles-form-widget' ).find( 'input.sort-is' );
+		setTimeout( refreshForm( el ), 1000 );
+		return false;
 	 } );
 
 	jQuery( '.show-count' ).click( function() {
