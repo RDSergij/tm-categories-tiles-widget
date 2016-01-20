2c269f4a55f9e0059008de77e614f83bbfe1c336
RDSergij <sergijlys@gmail.com>
2016-01-20 10:39:57 +0200

    Code style

--- views/themes/grid-1-2.php
+++ views/themes/grid-1-2.php
@@ -15,24 +15,25 @@ if ( ! defined( 'WPINC' ) ) {
 	<h3><?php echo $title ?></h3>
 	<div class="grid-wrap">
 		<?php foreach ( $categories as $index => $category ) : ?>
-		<?php if ( 2 < $index ) continue; ?>
-		<?php if ( $index ) : ?>
-		<?php $size = 'small' ?>
-		<?php else : ?>
-		<?php $size = 'big' ?>
-		<?php endif; ?>
-		<a href="<?php echo $category['url'] ?>">
-			<div class="cell-<?php echo $size ?>" 
-				<?php if ( ! empty( $category['image'] ) ) : ?>
-				style="background: url(<?php echo $category['image'] ?>) no-repeat;"
+			<?php if ( 2 >= $index ) : ?>
+				<?php if ( $index ) : ?>
+				<?php $size = 'small' ?>
+				<?php else : ?>
+				<?php $size = 'big' ?>
 				<?php endif; ?>
-				>
-				<div class="title"><?php echo $category['name'] ?></div>
-				<?php if ( 'true' == $show_count ) : ?>
-				<div class="count"><?php printf( __( '%d posts', PHOTOLAB_BASE_TM_ALIAS ), $category['count'] ); ?></div>
-				<?php endif; ?>
-			</div>
-		</a>
+				<a href="<?php echo $category['url'] ?>">
+					<div class="cell-<?php echo $size ?>" 
+						<?php if ( ! empty( $category['image'] ) ) : ?>
+						style="background: url(<?php echo $category['image'] ?>) no-repeat;"
+						<?php endif; ?>
+						>
+						<div class="title"><?php echo $category['name'] ?></div>
+						<?php if ( 'true' == $show_count ) : ?>
+						<div class="count"><?php printf( __( '%d posts', PHOTOLAB_BASE_TM_ALIAS ), $category['count'] ); ?></div>
+						<?php endif; ?>
+					</div>
+				</a>
+			<?php endif; ?>
 		<?php endforeach; ?>
 	</div>
 </div>
--- views/themes/line-3.php
+++ views/themes/line-3.php
@@ -15,19 +15,20 @@ if ( ! defined( 'WPINC' ) ) {
 	<h3><?php echo $title ?></h3>
 	<div class="grid-wrap">
 		<?php foreach ( $categories as $index => $category ) : ?>
-		<?php if ( 3 < $index ) continue; ?>
-		<a href="<?php echo $category['url'] ?>">
-			<div class="cell-small" 
-				<?php if ( ! empty( $category['image'] ) ) : ?>
-				style="background: url(<?php echo $category['image'] ?>) no-repeat;"
-				<?php endif; ?>
-				>
-				<div class="title"><?php echo $category['name'] ?></div>
-				<?php if ( 'true' == $show_count ) : ?>
-				<div class="count"><?php printf( __( '%d posts', PHOTOLAB_BASE_TM_ALIAS ), $category['count'] ); ?></div>
-				<?php endif; ?>
-			</div>
-		</a>
+			<?php if ( 3 < $index ) : ?>
+				<a href="<?php echo $category['url'] ?>">
+					<div class="cell-small" 
+						<?php if ( ! empty( $category['image'] ) ) : ?>
+						style="background: url(<?php echo $category['image'] ?>) no-repeat;"
+						<?php endif; ?>
+						>
+						<div class="title"><?php echo $category['name'] ?></div>
+						<?php if ( 'true' == $show_count ) : ?>
+						<div class="count"><?php printf( __( '%d posts', PHOTOLAB_BASE_TM_ALIAS ), $category['count'] ); ?></div>
+						<?php endif; ?>
+					</div>
+				</a>
+			<?php endif; ?>
 		<?php endforeach; ?>
 	</div>
 </div>
